6be2076eb0abfc9333e77bd026d2721bfb3cb74a
yannick <yannickkamdemkouam@yahoo.fr>
2022-05-11 16:56:14 +0200

    init Composant TP

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image style={styles.picture} source={require('./assets/rover.jpg')}/>
+      <View style={styles.text}>
+        <Text style={styles.title}>Title: Michael FARADAY</Text>
+        <Text style={styles.description}>Description: Michael Faraday (Newington, 22 septembre 1791 - Hampton Court, 25 août 1867) est un physicien et un chimiste britannique.</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 2,
+    flexDirection: "row",
   },
+
+  picture: {
+    flex: 1, 
+  },
+
+  title: {
+    margin: 10
+  },
+
+  description: {
+    margin: 10
+  },
+
+  text: {
+    flex: 1
+  }
 });
